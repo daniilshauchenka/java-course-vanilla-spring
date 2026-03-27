9c56f07eca5e1e5b33b40cffbdedea78628db12a
Шевченко Даниил Александрович <daniil.shauchenka@softclub.by>
2026-03-27 17:23:47 +0300

    added liquibase and configured h2 database connection

--- src/main/java/ru/yandex/Main.java
+++ src/main/java/ru/yandex/Main.java
@@ -4,15 +4,12 @@ import java.sql.SQLException;
 import org.h2.tools.Server;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.yandex.config.AppConfig;
-import ru.yandex.repository.TestRepo;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
         Server.createWebServer("-webPort", "8082").start();
         var context = new AnnotationConfigApplicationContext(AppConfig.class);
-
-        var repo = context.getBean(TestRepo.class);
-        repo.test();
+        System.out.println();
     }
 }
