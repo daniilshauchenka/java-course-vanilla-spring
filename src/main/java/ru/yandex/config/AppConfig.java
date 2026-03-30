00446cdd8c0f6a60c064b0f48e1b86d03fba7580
Шевченко Даниил Александрович <daniil.shauchenka@softclub.by>
2026-03-30 19:43:08 +0300

    started tomcat

--- src/main/java/ru/yandex/config/AppConfig.java
+++ src/main/java/ru/yandex/config/AppConfig.java
@@ -3,10 +3,13 @@ package ru.yandex.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
 @ComponentScan("ru.yandex")
+@Import(WebConfig.class)
 public class AppConfig {
 
     @Bean
---
+++ src/main/java/ru/yandex/config/WebConfig.java
@@ -0,0 +1,11 @@
+package ru.yandex.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig {
+}
