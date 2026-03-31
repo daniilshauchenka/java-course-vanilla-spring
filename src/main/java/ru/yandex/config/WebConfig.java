b1fe489eb6313edb24b6a0641d43ee713348694f
Шевченко Даниил Александрович <daniil.shauchenka@softclub.by>
2026-03-31 14:18:44 +0300

    cors settings stub

--- src/main/java/ru/yandex/config/WebConfig.java
+++ src/main/java/ru/yandex/config/WebConfig.java
@@ -1,11 +1,19 @@
 package ru.yandex.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowedHeaders("*");
+    }
 }
